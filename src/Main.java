c5bb97f64c6a6d1c4604a791334697d402c3a504
Marina Tislenko <26051978m@gmail.com>
2022-06-05 10:54:51 +0300

    first

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int money = 10_000; // цена билета
+        int rubles = 20; // 20 рублей за каждую милю
+        System.out.println("количество бонусных миль");
+        System.out.println(money/rubles);
+    }
